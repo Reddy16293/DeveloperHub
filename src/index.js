4be5f9bcd5f9bb7a9884ae1ef71455af4c936c6a
Reddy16293 <chilekampalli2004@gmail.com>
2024-06-29 21:12:37 +0530

    Backend Complete

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 const express= require('express');
 const dbconnect= require('./config/database.js');
-const {Devuser,Review} = require('./models');
-const jwt= require('jsonwebtoken');
-const middleware= require('./middleware.js');
+
+
 const cors =require('cors');
 const apiroutes=require('./router');
-
-
 
 
 const app=express();
@@ -16,26 +13,6 @@ app.use(cors({origin:'*'}));
 app.use(express.urlencoded({extended:true}));
 
 app.use('/api',apiroutes);
-
-app.get('/',(req,res)=>{
-    res.send('Welcome');
-})
-
-
-
-
-
-
-
-app.get('/myreview', middleware, async (req, res) => {
-    try {
-        let myreviews = await Review.find({ taskworker: req.user.id });
-        return res.status(200).json(myreviews);
-    } catch (error) {
-        console.log(error);
-        return res.status(500).send('Server Error');
-    }
-});
 
 app.listen(3000,async()=>{
     console.log('Server is running on port 3000');
